3a63178a4469f0f20abe0ca07d6eb3c85209cb20
Shane Mitchell <shanesc@gmail.com>
2021-05-06 15:36:06 +0100

    chore: lifted state up to App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+import { Component } from 'react';
 import Form from './components/Form';
 
-function App() {
-  return (
-    <div>
-      <Form />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      bio: {
+        name: '',
+        title: '',
+        phone: '',
+        email: '',
+        location: ''
+      }
+    };
+  }
+
+  handleChange = (key, e) => {
+    this.setState({
+      bio: { ...this.state.bio, [key]: e.target.value }
+    });
+    console.log(this.state.bio);
+  };
+
+  render() {
+    return (
+      <div>
+        <Form state={this.state} handleChange={this.handleChange} />
+      </div>
+    );
+  }
 }
 
 export default App;
