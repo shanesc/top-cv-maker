6da334a8296353bc35f8da37f8535ea9a5e3976c
Shane Mitchell <shanesc@gmail.com>
2021-05-11 12:16:37 +0100

    chore: remove unnecessary imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import data from './sample-cv-data';
 import PersonalInfo from './components/View/PersonalInfo';
 import './App.css';
-import ExperienceItem from './components/View/ExperienceItem';
 import ExperienceList from './components/View/ExperienceList';
 
 const { personal, experience: experienceList } = data;
