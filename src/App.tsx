6780e99797e3ca4b364e9b57e4a6e9124ca68f69
Shane Mitchell <shanesc@gmail.com>
2021-05-11 16:27:22 +0100

    feat: add CV detials state to App

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,91 @@ import './App.css';
 import ExperienceList from './components/View/ExperienceList';
 import Form from './components/Form';
 
-const { personal, description, experience: experienceList } = data;
-const educationList = data.education.map((item) => {
-  return {
-    position: item.course,
-    company: item.university,
-    startDate: item.startDate,
-    endDate: item.endDate
-  };
-});
+interface PersonalDetails {
+  name: string;
+  title: string;
+  phone: string;
+  email: string;
+  location: string;
+}
 
-class App extends Component {
+interface Experience {
+  id: number;
+  position: string;
+  company: string;
+  startDate: string;
+  endDate: string;
+  desc?: string;
+}
+
+interface Education {
+  id: number;
+  course: string;
+  university: string;
+  startDate: string;
+  endDate: string;
+  desc?: string;
+}
+interface State {
+  personalDetails: PersonalDetails;
+  description: string;
+  experience: Experience[];
+  education: Education[];
+}
+
+class App extends Component<{}, State> {
+  constructor() {
+    super({});
+
+    this.state = {
+      personalDetails: {
+        name: '',
+        title: '',
+        phone: '',
+        email: '',
+        location: ''
+      },
+      description: '',
+      experience: [],
+      education: []
+    };
+  }
+
+  componentDidMount() {
+    const { personal, description, experience, education } = data;
+    this.setState((prevState) => ({
+      ...prevState,
+      personalDetails: personal,
+      description,
+      experience,
+      education
+    }));
+  }
+
   render() {
+    const {
+      personalDetails,
+      description,
+      experience: experienceList,
+      education
+    } = this.state;
+
+    const educationList = education.map((item) => {
+      return {
+        position: item.course,
+        company: item.university,
+        startDate: item.startDate,
+        endDate: item.endDate
+      };
+    });
+
     return (
       <>
         <div className='cv-form'>
           <Form />
         </div>
         <div className='cv-container'>
-          <PersonalInfo {...personal} />
+          <PersonalInfo {...personalDetails} />
           <div>{description}</div>
           <ExperienceList
             heading='work experience'
