0850b05b4286e6035a44b70c9941d9549a613cfb
Shane Mitchell <shanesc@gmail.com>
2021-05-11 12:38:55 +0100

    feat: add education list to CV

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,25 @@ import './App.css';
 import ExperienceList from './components/View/ExperienceList';
 
 const { personal, experience: experienceList } = data;
+const educationList = data.education.map((item) => {
+  return {
+    position: item.course,
+    company: item.university,
+    startDate: item.startDate,
+    endDate: item.endDate
+  };
+});
 
 class App extends Component {
   render() {
     return (
       <div className='cv-container'>
         <PersonalInfo {...personal} />
-        <ExperienceList experienceList={experienceList} />
+        <ExperienceList
+          heading='work experience'
+          experienceList={experienceList}
+        />
+        <ExperienceList heading='education' experienceList={educationList} />
       </div>
     );
   }
